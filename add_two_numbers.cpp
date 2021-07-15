78119ca32e3cbb163907f883bddf50554444dd85
Darren Ng <darren.ng@versed.ai>
2021-07-15 21:24:52 +0100

    updated add_two_numbers.cpp

--- add_two_numbers.cpp
+++ add_two_numbers.cpp
@@ -13,17 +13,32 @@ int main()
     // The result is the ostream on which we wrote the given value
     // The result of first operator becomes the helf-hand operand of the second
     // chaining together output requests
-    std::cout << "Enter two numbers: " << std::endl;
-    // This is identical to 
+    // This is equivalent to 
     // std::cout << "Enter two numbers:";
     // std::cout << std::endl;
+    std::cout << "Enter two numbers: " << std::endl;
 
     // define 2 integer type variables and initialise as 0
     int v1 = 0, v2 = 0;
 
     // The input operator >> taktes istream as left-hand operand
+    // This is equivalent to
+    // std::cin >> v1;
+    // std::cin >> v2;
     std::cin >> v1 >> v2;
+    
+    // This is equivalent to
+    // std::cout << "The sum of " << v1;
+    // std::cout << " and " << v2;
+    // std::cout << " is " << v1 + v2 << std::endl;
     std::cout << "The sum of " << v1 << " and " << v2
               << " is " << v1 + v2 << std::endl;
     return 0;
+
+    /* This
+    is
+    a
+    multiline
+    comment
+    */
 }
